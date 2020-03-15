a999f759369d8e07b9b65a95dbec14a9842da197
YUKIKO IKEDA <applelele@gmail.com>
2020-03-15 21:54:21 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,18 +7,23 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A\w+un/ ) && text
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]\w+,\s\b/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
+  phone.match(/\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/)
 
 end
